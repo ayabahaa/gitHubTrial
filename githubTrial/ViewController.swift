619dae39433246b117430ace3b105cb60424f95d
sayed abdo <sayed.m.abdo123@gmail.com>
2020-07-07 00:52:24 +0200

    ya rab :)

--- githubTrial/ViewController.swift
+++ githubTrial/ViewController.swift
@@ -14,13 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("thanks")
         print("aya11")
-<<<<<<< HEAD
-        
-        
-=======
         print(2222)
         print("ffffffffffffffffffffffsfsgsssgshdurhfehrflre")
->>>>>>> aya
     }
 
 
