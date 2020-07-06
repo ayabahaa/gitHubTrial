43f30f031cff6274e938ef8f363e2f48dfb824cb
Aya Bahaa <ayabahaa24@yahoo.com>
2020-07-07 00:24:52 +0200

    delete

--- githubTrial/ViewController.swift
+++ githubTrial/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("thanks")
         print("aya11")
-        print(2222)
+        
+        
     }
 
 
